a7ed0c5d59432228208a54e31379f45a3489ab92
Martin Johnson <M.J.Johnson@massey.ac.nz>
2023-10-09 13:12:24 +1300

    Add Audio to Jobsworth app

--- Jobsworth/app/src/main/java/nz/massey/jobsworth/MainActivity.java
+++ Jobsworth/app/src/main/java/nz/massey/jobsworth/MainActivity.java
@@ -9,11 +9,13 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import nz.massey.jobsworth.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mMp;
     public ActivityMainBinding mMainLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,22 @@ public class MainActivity extends AppCompatActivity {
         if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 0);
         }
-        mMainLayout.intentButton.setOnClickListener((view ->
+        mMainLayout.intentButton.setOnClickListener(view ->
             startService(new Intent(this,MyIntentService.class))
-        ));
-        mMainLayout.jobIntentButton.setOnClickListener((view ->
+        );
+        mMainLayout.jobIntentButton.setOnClickListener(view ->
                 JobIntentService.enqueueWork(this, MyJobIntentService.class, 159336, new Intent())
-        ));
-        mMainLayout.jobButton.setOnClickListener((view ->
-                MyJobService.scheduleJob(this)
-        ));
+        );
+        mMainLayout.jobButton.setOnClickListener(view -> MyJobService.scheduleJob(this));
+        mMp=MediaPlayer.create(this,R.raw.example);
+        mMp.setLooping(true);
+        mMainLayout.soundButton.setOnClickListener(view -> {
+            if(mMp.isPlaying()) mMp.pause(); else mMp.start();
+        } );
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMp.pause();
     }
 }
